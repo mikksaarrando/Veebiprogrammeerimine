1199c8de8dd6aef482503bd49a3655845c9a7060
Rando Mikksaar <mikksaarrando@users.noreplay.github.com>
2017-09-12 14:15:40 +0300

    Lisatud on esimene jupp PHP koodi

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Rando Mikksaar veebiprogrammeerimine</title>
+</head>
+<body>
+	<h1>Rando Mikksaar veebiprogrammeerimine</h1>
+	<p>see lehekülg on loodud Õppetöö eesmärgil</p>
+	
+	<?php
+		echo "See on esimene jupp PHP abil väljastatud teksti.";
+	
+	?>
+	
+</body>
+</html>
